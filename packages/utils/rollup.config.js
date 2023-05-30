37762c5beef4f245c5b322bbaf5d24b50fa68d0b
mudassirNeosoft <root@Neosofts-MacBook-Pro.local>
2023-05-30 15:21:04 +0530

    remove commented code

--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -2,14 +2,11 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-// import postcss from "rollup-plugin-postcss";
 import json from "@rollup/plugin-json";
 
 const packageJson = require("./package.json");
 const external = [
-  ...Object.keys(packageJson.dependencies || {}),
   ...Object.keys(packageJson.devDependencies || {}),
-  ...Object.keys(packageJson.peerDependencies || {}),
 ];
 export default [
   {
@@ -39,6 +36,5 @@ export default [
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    // external: [/\.css$/],
   },
 ];
